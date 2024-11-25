64a25d5c4cf76dbc1f04f8d56517fe16a491f1c1
TazmanianTad <bittiez@gmail.com>
2024-11-24 20:44:34 -0700

    Fix & symbol in json save file

--- src/ClassicUO.Client/Game/UI/Gumps/ModernOptionsGump.cs
+++ src/ClassicUO.Client/Game/UI/Gumps/ModernOptionsGump.cs
@@ -6348,7 +6348,7 @@ namespace ClassicUO.Game.UI.Gumps
                     else
                         Option.NameOverheadOptionFlags &= ~(int)optionFlag;
 
-                    if (NameOverHeadManager.LastActiveNameOverheadOption == Option.Name)
+                    if (NameOverHeadManager.LastActiveNameOverheadOption.Replace("\\u0026", "&") == Option.Name)
                         NameOverHeadManager.ActiveOverheadOptions = (NameOverheadOptions)Option.NameOverheadOptionFlags;
                 });
 
